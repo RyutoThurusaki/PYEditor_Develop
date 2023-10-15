6c85d32e8048c81cae7a56d29d32d685e83f910e
uozumi <41750783+RyutoThurusaki@users.noreply.github.com>
2023-10-15 22:28:11 +0900

    3Dカーソルだいたい実装 (#21)
    ひとまず土台
    
    いろいろ追加実装
    
    設定を自動保存に
    
    カーソルの位置に小ウィンドウが出るように
    
    不要な部分を削除
    
    コメント追加
    
    3Dカーソルを追加
    
    Assets/Gizmosにアセットを追加する処理を追加
    
    Infoの表示機能&コメントの追加
    
    必要Objectの生成と削除機能、ユーザー補助機能の追加

---
+++ Editor/3DCursor/ThreeDCursor_DrawGizmo.cs
@@ -0,0 +1,24 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreeDCursor_DrawGizmo : MonoBehaviour
+{
+    [HideInInspector]
+    public Vector3 center = Vector3.zero;
+    [HideInInspector]
+    public Boolean isAutogenerate = false;
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawIcon(center, "3DCursor", false);
+    }
+
+    public void EraseThis()
+    {
+        GameObject.DestroyImmediate(transform.gameObject);
+    }
+}
+
+#endif
